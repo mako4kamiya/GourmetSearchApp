a0cd7513088cff98a08333948e64b7874be3c45a
mako4kamiya <mako4kamiya@gmail.com>
2021-05-04 18:06:32 +0900

    Hopeコンポーネントの作成 #12

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>Hello World! </h1>
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
