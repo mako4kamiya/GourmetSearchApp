8ee5012d1e40809cec85d477983140edf6092b96
mako4kamiya <mako4kamiya@gmail.com>
2021-05-04 20:02:40 +0900

    mapboxの導入#12

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,26 @@
-function Home(params) {
+import { useRef, useEffect, useState } from "react";
+import "../css/home.css";
+import mapboxgl from 'mapbox-gl';
+// import 'mapbox-gl/dist/mapbox-gl.css'
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFrbzRrYW1peWEiLCJhIjoiY2tvOXRiMGl5MmtydjJwcXc4cXN6cTNmbiJ9.25AHY8Eey5RTzOXyIvVC3A';
+
+function Home() {
+    const mapContainer = useRef();
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+        return () => map.remove();
+    }, []);
+
     return (
-        <h1>This is Home page</h1>
+    <div className="map-container" ref={mapContainer}></div>
     );
 }
 export default Home;
