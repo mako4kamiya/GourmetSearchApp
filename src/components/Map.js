a4d17a74b5804222f790f4085d20dcdd5eeab01f
mako4kamiya <mako4kamiya@gmail.com>
2021-05-05 16:52:59 +0900

    本番環境で位置情報取れてなかった問題 #12

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,22 +7,22 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFrbzRrYW1peWEiLCJhIjoiY2tvOXRiMGl5MmtydjJwc
 function Map() {
     const mapContainer = useRef();
     const style = "mapbox://styles/mako4kamiya/cko9xdnpd0nlq18qbpf0c787x";
-    // const [lng, setLng] = useState(127.95762682904041);
-    // const [lat, setLat] = useState(26.687547737468357);
     const [lng, setLng] = useState('');
     const [lat, setLat] = useState('');
 
-    function success(Position) {
-        setLng(Position.coords.longitude);
-        setLat(Position.coords.latitude);
-    }
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success);
-    } else {
-    //Geolocation APIを利用できない環境向けの処理
-    }
-
     useEffect(() => {
+        let getPotision = new Promise(function(resolve, reject){
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        })
+        getPotision
+        .then(res => {
+            setLng(res.coords.longitude);
+            setLat(res.coords.latitude);
+        })
+        .catch(err => {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        });
+
         const map = new mapboxgl.Map({
             container: mapContainer.current,
             style: style,
