b3126b070e6bafc0662b6b1352d72bdf65941b51
mako4kamiya <mako4kamiya@gmail.com>
2021-05-05 18:00:15 +0900

    本番環境で現在地の取得ができてない問題 #12

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,27 +9,26 @@ function Map() {
     const style = "mapbox://styles/mako4kamiya/cko9xdnpd0nlq18qbpf0c787x";
     const [lng, setLng] = useState('');
     const [lat, setLat] = useState('');
-    console.log(lng);
 
-    let getPotision = new Promise(function(resolve, reject){
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    })
-    getPotision
-    .then(res => {
-        console.log(res);
-        setLng(res.coords.longitude);
-        setLat(res.coords.latitude);
-    })
-    .catch(err => {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    });
+    // let getPotision = new Promise(function(resolve, reject){
+    //     navigator.geolocation.getCurrentPosition(resolve, reject);
+    // })
+    // getPotision
+    // .then(res => {
+    //     setLng(res.coords.longitude);
+    //     setLat(res.coords.latitude);
+    // })
+    // .catch(err => {
+    //     console.warn(`ERROR(${err.code}): ${err.message}`);
+    // });
 
     useEffect(() => {
         console.log(lng);
         const map = new mapboxgl.Map({
             container: mapContainer.current,
             style: style,
-            center: [lng, lat],
+            // center: [lng, lat],
+            center: [127.80064329999998, 26.3390386],
             zoom: "13"
         });
         return () => map.remove();
