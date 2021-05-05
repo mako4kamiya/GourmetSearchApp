614e2c3ec4860d629a9bf2f65ce6a7b8cf7f6f91
mako4kamiya <mako4kamiya@gmail.com>
2021-05-05 18:45:42 +0900

    本番環境で現在地の取得ができてない問題 #12

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,26 @@
-// import { useRef, useEffect, useState } from "react";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import "../css/home.css";
 import mapboxgl from 'mapbox-gl';
-// import 'mapbox-gl/dist/mapbox-gl.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFrbzRrYW1peWEiLCJhIjoiY2tvOXRiMGl5MmtydjJwcXc4cXN6cTNmbiJ9.25AHY8Eey5RTzOXyIvVC3A';
 
 function Map() {
     const mapContainer = useRef();
     const style = "mapbox://styles/mako4kamiya/cko9xdnpd0nlq18qbpf0c787x";
-    // const [lng, setLng] = useState('');
-    // const [lat, setLat] = useState('');
-    const lng = 127.80064329999998;
-    const lat = 26.3390386;
+    const [lng, setLng] = useState('');
+    const [lat, setLat] = useState('');
 
-    // let getPotision = new Promise(function(resolve, reject){
-    //     navigator.geolocation.getCurrentPosition(resolve, reject);
-    // })
-    // getPotision
-    // .then(res => {
-    //     setLng(res.coords.longitude);
-    //     setLat(res.coords.latitude);
-    // })
-    // .catch(err => {
-    //     console.warn(`ERROR(${err.code}): ${err.message}`);
-    // });
+    let getPotision = new Promise(function(resolve, reject){
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+    getPotision
+    .then(res => {
+        setLng(res.coords.longitude);
+        setLat(res.coords.latitude);
+    })
+    .catch(err => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    });
 
     useEffect(() => {
         console.log(lng);
@@ -31,7 +28,6 @@ function Map() {
             container: mapContainer.current,
             style: style,
             center: [lng, lat],
-            // center: [127.80064329999998, 26.3390386],
             zoom: "13"
         });
         return () => map.remove();
