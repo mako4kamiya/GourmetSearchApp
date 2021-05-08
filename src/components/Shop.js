73075d56b79ea66fe8ffe02449b46080ea39b65e
mako4kamiya <mako4kamiya@gmail.com>
2021-05-09 05:52:55 +0900

    shopコンポーネントに分けた #20

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,56 +1,53 @@
 import { useEffect, useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
 import SwipeableViews from 'react-swipeable-views';
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Typography from '@material-ui/core/Typography';
-
-const styles = {
-  root: {
-    padding: '0 30px',
-  },
-  slideContainer: {
-    padding: '0 10px',
-  },
-  slide: {
-    padding: 15,
-    minHeight: 100,
-    color: '#fff',
-  },
-  bgColor: {
-    backgroundColor: '#FEA900',
-  },
-};
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Chip from "@material-ui/core/Chip";
+import DoneIcon from "@material-ui/icons/Done";
+import "../css/shop.css";
 
 function Shop(props) {
   const [shops, setShops] = useState([]);
+  const shopComponents = (shops || []).map(shop =>(
+    <div className="card" key={shop.id}>
+        <Card>
+            <CardActionArea>
+                <CardMedia image={shop.photo.pc.l} title={shop.name}/>
+                <CardContent>
+                    <h2>{shop.name}</h2>
+                    <span className="open">{shop.address}</span>
+                    <p>{shop.open}</p>
+                    <div className="chips">
+                        {(shop.child.includes("お子様連れOK"))||(shop.child.includes("お子様連れ歓迎"))?
+                            <Chip className="chip" size="small" label="お子様連れ対応" color="secondary" icon={<DoneIcon />}/>
+                        :
+                            <Chip className="chip" size="small" label="お子様連れ対応なし" />
+                        }
+                        {shop.parking.startsWith("あり")?
+                            <Chip className="chip" size="small" label="駐車場あり" color="secondary" icon={<DoneIcon />}/>
+                        :
+                            <Chip className="chip" size="small" label="駐車場なし"/>
+                        }
+                    </div>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    </div>
+));
 
   useEffect(()=>{
     setShops(props.shops);
-    console.log(shops);
-  },[props]);
-
-
-  // const [id, setId] = useState(props.id);
-  // const [address, setAddress] = useState(props.address);
-  // const [photo, setPhoto] = useState(props.photo);
-  // const [logoImg, setLgoImg] = useState(props.logoImg);
-  // const [open, setOpen] = useState(props.open);
-  // const [close, setClose] = useState(props.close);
-  // const [creditCard, setCreditCard] = useState(props.creditCard);
-  // const [urls, setUrls] = useState(props.urls);
-  // const [parking, setParking] = useState(props.parking);
-  // const [child, setChild] = useState(props.child);
-  // const [privateRoom, setPrivateRoom] = useState(props.privateRoom);
+  },);
+  console.log(shops);
 
   return (
-    <SwipeableViews style={styles.root} slideStyle={styles.slideContainer}>
-      <div style={Object.assign({}, styles.slide, styles.baColor)}>slide n°1</div>
-      <div style={Object.assign({}, styles.slide, styles.baColor)}>slide n°2</div>
-      <div style={Object.assign({}, styles.slide, styles.baColor)}>slide n°3</div>
-    </SwipeableViews>
+    <div id="Shop">
+      <SwipeableViews className="swipe">
+        {shopComponents}
+      </SwipeableViews>
+    </div>
   );
 }
 export default Shop;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,7 @@ import Header from "../components/Header";
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import FilterNoneIcon from '@material-ui/icons/FilterNone';
 import Map from "../components/Map";
-// import Shop from "../components/Shop";
-
-
-import SwipeableViews from 'react-swipeable-views';
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Chip from "@material-ui/core/Chip";
-import DoneIcon from "@material-ui/icons/Done";
-
+import Shop from "../components/Shop";
 import "../css/home.css";
 
 function Home() {
@@ -26,33 +16,6 @@ function Home() {
     const headerLeft = <FormatListBulletedIcon/>;
     const headerLeftPath = "/list";
     const headerRight = <FilterNoneIcon />;
-
-    const shopComponents = (shops || []).map(shop =>(
-        <div className="card" key={shop.id}>
-            <Card>
-                <CardActionArea>
-                    <CardMedia image={shop.photo.pc.l} title={shop.name}/>
-                    <CardContent>
-                        <h2>{shop.name}</h2>
-                        <span className="open">{shop.address}</span>
-                        <p>{shop.open}</p>
-                        <div className="chips">
-                            {(shop.child.includes("お子様連れOK"))||(shop.child.includes("お子様連れ歓迎"))?
-                                <Chip className="chip" size="small" label="お子様連れ対応" color="secondary" icon={<DoneIcon />}/>
-                            :
-                                <Chip className="chip" size="small" label="お子様連れ対応なし" />
-                            }
-                            {shop.parking.startsWith("あり")?
-                                <Chip className="chip" size="small" label="駐車場あり" color="secondary" icon={<DoneIcon />}/>
-                            :
-                                <Chip className="chip" size="small" label="駐車場なし"/>
-                            }
-                        </div>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </div>
-    ));
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((res) => {
@@ -79,11 +42,7 @@ function Home() {
                 headerRight={headerRight}
             />
             <Map lng={lng} lat={lat}/>
-            <div id="Shop">
-                <SwipeableViews className="swipe">
-                    {shopComponents}
-                </SwipeableViews>
-            </div>
+            <Shop shops={shops}/>
         </div>
     );
 }
