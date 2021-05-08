cd838c8c732c12300945c1c748ff40ed7dd22450
mako4kamiya <mako4kamiya@gmail.com>
2021-05-09 01:18:53 +0900

    Home画面カードの表示以外終了 #17

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,89 +1,56 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableViews from 'react-swipeable-views';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+// import Card from '@material-ui/core/Card';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import Typography from '@material-ui/core/Typography';
 
-const shops = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+const styles = {
+  root: {
+    padding: '0 30px',
   },
-  {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  slideContainer: {
+    padding: '0 10px',
   },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
   },
-  {
-    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+  bgColor: {
+    backgroundColor: '#FEA900',
   },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-];
-
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
-  },
-  cardMedia: {
-    potision: "static",
-  },
-});
+};
 
 function Shop(props) {
-  const [lng, setLng] = useState(props.lng);
-  const [lat, setLat] = useState(props.lat);
+  const [shops, setShops] = useState([]);
 
-  useEffect(() => {
-    axios.get(`/gourmet/v1/?key=4c8ae073fc977810&lat=${props.lat}&lng=${props.lng}&format=json`)
-    .then((res)=>{
-      console.log(res);
-      });
-  }, [lng, lat, props]);
+  useEffect(()=>{
+    setShops(props.shops);
+    console.log(shops);
+  },[props]);
 
 
-  const classes = useStyles();
+  // const [id, setId] = useState(props.id);
+  // const [address, setAddress] = useState(props.address);
+  // const [photo, setPhoto] = useState(props.photo);
+  // const [logoImg, setLgoImg] = useState(props.logoImg);
+  // const [open, setOpen] = useState(props.open);
+  // const [close, setClose] = useState(props.close);
+  // const [creditCard, setCreditCard] = useState(props.creditCard);
+  // const [urls, setUrls] = useState(props.urls);
+  // const [parking, setParking] = useState(props.parking);
+  // const [child, setChild] = useState(props.child);
+  // const [privateRoom, setPrivateRoom] = useState(props.privateRoom);
 
   return (
-    <div id="Shop">
-      <SwipeableViews>
-        {shops.map((shop) => (
-          <div key={shop.label}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  image={shop.imgPath}
-                  title="Contemplative Reptile"
-                  className={classes.cardMedia}
-                />
-                <CardContent>
-                  <Typography>{shop.label}</Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                  {shop.label}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </div>
-        ))}
-      </SwipeableViews>
-    </div>
+    <SwipeableViews style={styles.root} slideStyle={styles.slideContainer}>
+      <div style={Object.assign({}, styles.slide, styles.baColor)}>slide n°1</div>
+      <div style={Object.assign({}, styles.slide, styles.baColor)}>slide n°2</div>
+      <div style={Object.assign({}, styles.slide, styles.baColor)}>slide n°3</div>
+    </SwipeableViews>
   );
 }
 export default Shop;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,57 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../components/Header";
-import Map from "../components/Map";
-import Shop from "../components/Shop";
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import FilterNoneIcon from '@material-ui/icons/FilterNone';
-import "../css/home.css";
+import Map from "../components/Map";
+// import Shop from "../components/Shop";
 
-const headerTitle = "周辺の検索";
-const headerLeft = <FormatListBulletedIcon/>;
-const headerLeftPath = "/list";
-const headerRight = <FilterNoneIcon />;
+
+import SwipeableViews from 'react-swipeable-views';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Chip from "@material-ui/core/Chip";
+import DoneIcon from "@material-ui/icons/Done";
+
+import "../css/home.css";
 
 function Home() {
     const [lng, setLng] = useState(null);
     const [lat, setLat] = useState(null);
     const [shops, setShops] = useState([]);
 
+    const headerTitle = "周辺の検索";
+    const headerLeft = <FormatListBulletedIcon/>;
+    const headerLeftPath = "/list";
+    const headerRight = <FilterNoneIcon />;
+
     const shopComponents = (shops || []).map(shop =>(
-        <Shop shops={shops}/>
+        <div className="card" key={shop.id}>
+            <Card>
+                <CardActionArea>
+                    <CardMedia image={shop.photo.pc.l} title={shop.name}/>
+                    <CardContent>
+                        <h2>{shop.name}</h2>
+                        <span className="open">{shop.address}</span>
+                        <p>{shop.open}</p>
+                        <div className="chips">
+                            {(shop.child.includes("お子様連れOK"))||(shop.child.includes("お子様連れ歓迎"))?
+                                <Chip className="chip" size="small" label="お子様連れ対応" color="secondary" icon={<DoneIcon />}/>
+                            :
+                                <Chip className="chip" size="small" label="お子様連れ対応なし" />
+                            }
+                            {shop.parking.startsWith("あり")?
+                                <Chip className="chip" size="small" label="駐車場あり" color="secondary" icon={<DoneIcon />}/>
+                            :
+                                <Chip className="chip" size="small" label="駐車場なし"/>
+                            }
+                        </div>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </div>
     ));
 
     useEffect(() => {
@@ -46,7 +79,11 @@ function Home() {
                 headerRight={headerRight}
             />
             <Map lng={lng} lat={lat}/>
-            {shopComponents}
+            <div id="Shop">
+                <SwipeableViews className="swipe">
+                    {shopComponents}
+                </SwipeableViews>
+            </div>
         </div>
     );
 }
