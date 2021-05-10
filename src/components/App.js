1e730d20cc1c06c6ab4fdf0124af30f77b140f10
mako4kamiya <mako4kamiya@gmail.com>
2021-05-11 05:43:52 +0900

    App.js効率化

--- src/components/App.js
+++ src/components/App.js
@@ -6,50 +6,54 @@ import Filter from "../pages/Filter";
 import Home from "../pages/Home";
 
 const KEY = process.env.REACT_APP_HOTPEPPER_API_KEY
+// const REQUEST_URL = `https://cors-for-gourmet-search-app.herokuapp.com/http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${KEY}&format=json&type=credit_card&range=5`
+const REQUEST_URL = `https://cors-for-gourmet-search-app.herokuapp.com/http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${KEY}&format=json&type=credit_card`
 
 function App() {
     const [lng, setLng] = useState("");
     const [lat, setLat] = useState("");
-    const [shops, setShops] = useState([]);
     const [baseUrl, setBaseUrl]  = useState("");
-    const [query, setQuery] = useState("");
-    const [location, setLocation] = useState(false);
-    // console.log(lng, lat, query);
+    const [query, setQuery] = useState(false);
+    const [option, setOption] = useState("");
+    const [shops, setShops] = useState([]);
 
-    function createQuery(obj) {
-        let newUrl = baseUrl;
-        for (const [key, value] of Object.entries(obj)) {
-            const options = `&${key}=${value}`;
-            newUrl += options;
+    function getLocation(option) {
+        return new Promise(function(resolve, reject, option) {
+            navigator.geolocation.getCurrentPosition(resolve, reject, option);
+        });
+    }
+    async function createBaseState() {
+        try {
+            let Location = await getLocation();
+            let lng = Location.coords.longitude;
+            let lat = Location.coords.latitude;
+            let baseUrl = `${REQUEST_URL}&lat=${lat}&lng=${lng}`;
+            setLng(lng);
+            setLat(lat);
+            setBaseUrl(baseUrl);
+            setQuery(baseUrl);
+        } catch(err) {
+            console.log(err);
         }
-        setQuery(newUrl);
-        console.log(query);
+    }
+    async function getShopInfo(query,option) {
+        try {
+            let res = await axios.get(query + option);
+            setShops(res.data.results.shop);
+        } catch(err) {
+            console.log(err);
+        }
     }
 
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition((res) => {
-            setLng(res.coords.longitude);
-            setLat(res.coords.latitude);
-            setLocation(true);
-        });
+        createBaseState();
     },[]);
 
     useEffect(()=>{
-        setBaseUrl(`https://cors-for-gourmet-search-app.herokuapp.com/http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${KEY}&lat=${lat}&lng=${lng}&format=json&type=credit_card&range=5`);
-        setQuery(`https://cors-for-gourmet-search-app.herokuapp.com/http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${KEY}&lat=${lat}&lng=${lng}&format=json&type=credit_card&range=5`);
-    },[lng, lat]);
-
-    useEffect(()=>{
-        if(location){
-            axios.get(`${query}`)
-                .then((res)=>{
-                // console.log(res);
-                setShops(res.data.results.shop);
-            });
+        if (query) {
+            getShopInfo(query, option);
         }
-        // const obj = {'a': 1, 'b': 2, 'c': 3};
-        // createQuery(obj);
-    },[query]);
+    },[query, option])
 
     return (
         <Switch>
@@ -57,7 +61,7 @@ function App() {
                 <List shops={shops}/>
             </Route>
             <Route path="/filter">
-                <Filter shops={shops} createQuery={createQuery}/>
+                <Filter shops={shops} setOption={setOption}/>
             </Route>
             <Route path="/">
                 <Home lng={lng} lat={lat} shops={shops}/>
